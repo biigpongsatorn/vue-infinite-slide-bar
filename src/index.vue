13b258f3c77110c8556484b732a6280a1ace02ee
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-05-26 01:29:28 +0700

    🎉 Initial commit

---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello World 🌍 </h1><br>
+    <span> Welcome to vue-infinite-slider 👏🏻 </span><br>
+    <span>📝 Infinite slider bar component.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'vue-infinite-slider',
+}
+</script>
+
+<style scoped>
+</style>
