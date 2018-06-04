2ec0cf4f840035cdd18fa404e4d1dd4b7708f195
biig_pongsatorn <biig_pongsatorn@hotmail.com>
2018-06-04 10:22:28 +0700

    ♻️ Refactoring code

--- src/index.vue
+++ src/index.vue
@@ -1,33 +1,11 @@
 <template>
   <div class="container">
     <div class="slide">
-      <div class="list--container">
-        <div class="list--li-box" v-for="i in 5" :key="i">
-          <img class="list--coin-img" src="https://s2.coinmarketcap.com/static/img/coins/32x32/1.png">
-          <div class="list--coin-box">
-            <div class="list--coin-name">
-              Bitcoin (BTC)
-            </div>
-            <div class="list--coin-detail">
-              $16,815.10 
-              <span class="has-text-success">(11.68%)</span>
-            </div>
-          </div>
-        </div>
+      <div class="list--container" style="background-color: red;color: white;">
+        <div>Helo oewjhksajdoskad;lsa;'dsads</div>
       </div>
-      <div class="list--container">
-        <div class="list--li-box" v-for="i in 5" :key="i">
-          <img class="list--coin-img" src="https://s2.coinmarketcap.com/static/img/coins/32x32/1.png">
-          <div class="list--coin-box">
-            <div class="list--coin-name">
-              Bitcoin (BTC)
-            </div>
-            <div class="list--coin-detail">
-              $16,815.10 
-              <span class="has-text-success">(11.68%)</span>
-            </div>
-          </div>
-        </div>
+      <div class="list--container" style="background-color: green;color: white;">
+        <div>Helo oewjhksajdoskad;lsa;'dsads</div>
       </div>
     </div>
   </div>
@@ -43,28 +21,7 @@ export default {
 .list--container {
   float: left;
   width: 50%;
-  display: flex;
-  justify-content: space-between;
-}
-.list--li-box {
-  float: left;
-  padding: 0 20px;
-}
-.list--coin-img {
-  float: left;
-  margin-top: 5px;
-  width: 32px;
-  height: 32px;
-}
-.list--coin-box {
-  float: left;
-  padding-left: 10px;
-}
-.list--coin-name {
-  font-weight: bold;
-}
-.list--coin-detail {
-  font-size: 11px;
+  height: 100%;
 }
 
 @keyframes moveSlideshow {
@@ -77,14 +34,11 @@ export default {
   height: 60px;
   background-color: #ffffff;
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.06);
-  margin-bottom: 20px;
   position: relative;
-  padding: 10px;
   overflow: hidden;
 }
 .slide {
   transform: translate3d(0, 0, 0); /* Hey browser, use your GPU */
-  height: 50px;
   width: 200%;
   position: absolute;
   top: 10px;
